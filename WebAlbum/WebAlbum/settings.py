c396841be56b086e3dbf73379fe02c470b8cedfb
Leonardo Wainer <leowainer.lw@gmail.com>
2021-12-14 17:07:20 -0300

    adicao .env

--- WebAlbum/WebAlbum/settings.py
+++ WebAlbum/WebAlbum/settings.py
@@ -124,16 +124,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# MEDIA_URL = '/images/'
 
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
-DEFAULT_FILE_STORAGE = env('DEFAULT_FILE_STORAGE')
-AWS_S3_ACCESS_KEY_ID = env('AWS_S3_ACCESS_KEY_ID')
-AWS_S3_SECRET_ACCESS_KEY = env('AWS_S3_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+# MEDIA_ROOT = BASE_DIR/ 'static/images'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_ACCESS_KEY_ID = env("AWS_S3_ACCESS_KEY_ID")
+AWS_S3_SECRET_ACCESS_KEY = env("AWS_S3_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+
 
 AWS_QUERYSTRING_AUTH = False
-
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
